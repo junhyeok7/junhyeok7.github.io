afcfd1fdf3e16304dc93029d7e76e7bf6bf068cc
junhyeok7 <65381295+junhyeok7@users.noreply.github.com>
2023-07-26 02:07:57 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("hello there");
+var decrypt = CryptoJS.enc.Base64.parse("MTY3NzcyMTY");
+var hashData = decrypt.toString(CryptoJS.enc.Utf8);
+
+function printName()  {
+    const idvalue = document.getElementById('id').value;
+    const pwvalue = document.getElementById('pw').value;
+    if (idvalue == "admin"){
+        if (pwvalue == hashData){
+            var win = window.open("file:///C:/Users/pc/Desktop/index.html");
+        }
+    }
+  }
+
+//16777216
